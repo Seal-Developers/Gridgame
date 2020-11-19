0b7b0eb8885792140cdd0fbbc57c46168375bd0e
Arya2504s <73757085+Arya2504s@users.noreply.github.com>
2020-11-19 08:45:24 +0530

    COmpleted draw_Gird

--- src/components/draw_Grid.java
+++ src/components/draw_Grid.java
@@ -4,16 +4,26 @@ import java.util.ArrayList;
 
 
 public class draw_Grid{
-  static int pos_i = 2, pos_j = 2;
-  static int [][]Adjmat = {{1,1,1},{0,0,1},{0,0,1}};
+  static int pos_i = 4, pos_j = 4, venemy = 4, henemy = 3;
+  static int [][]Adjmat = {{1,0,0,1,0},{1,0,1,0,1,},{1,1,0,1,0,},{0,1,1,1,0},{1,0,1,1,1}};
   ArrayList<Integer> wall = new ArrayList<Integer>();
   public ArrayList<Integer> getwalls(int [][]Adjmat){
     
     return wall;
   }
-  public static void main(String[] args) {
-    
-    int row = 3, col = 3;
+  public static void print_dash(int row) {
+    for(int i =0 ;i< row;i++) {
+      if( i != row - 1) {
+        System.out.print("___ ");
+      }
+    }
+  }
+  public static void grid_draw(int row, int pos) {
+    int pos_i = pos/row, pos_j = pos%row; 
+    if(pos_i >= row || pos_j>= row) {
+      System.out.println("Out of bounds");
+      return;
+    }
     for(int i = 0; i < row; i++ ) {
       System.out.print(" ");
       for(int j =0 ;j< row;j++) {
@@ -28,9 +38,9 @@ public class draw_Grid{
         }
       }
       System.out.println();
-      for(int j =0;j < col;j++) {
+      for(int j =0;j < row;j++) {
         
-       if(j!= 0 && Adjmat[i][j] == 1 && Adjmat[i][j-1] == 1) {
+       if(j!= 0 && Adjmat[i][j] == 1 && Adjmat[i][j-1] == 1 ) {
          if(pos_i == i && pos_j == j) {
            System.out.print(" c  ");
          }
@@ -50,11 +60,28 @@ public class draw_Grid{
       System.out.println("|");
     }
     System.out.print(" ");
-    for(int i =0 ;i< row;i++) {
-      if( i != row - 1) {
-        System.out.print("___ ");
-      }
-    }
+    print_dash(row);
   }
-    
+  public static void main(String args[]) {
+    int pos = 0;
+    grid_draw(5,0);
+    System.out.println();
+     for(int i =0;i<5;i++) { 
+       for(int j=0;j<5;j++) { 
+         if(i!= 0 && Adjmat[i][j] == 1 && Adjmat[i-1][j] == 1 && (pos % 5)<= j) {
+           pos = i*5+j;
+           grid_draw(5,pos);
+           System.out.println();
+         }
+         else if(j!= 0 && Adjmat[i][j] == 1 && Adjmat[i][j-1] == 1&& (pos_i/ 5)<= i) {
+           pos = i*5+j;
+           grid_draw(5,pos);
+           System.out.println();
+         }
+         
+       }
+    }
+     System.out.println();
+     grid_draw(5,60);
+  }
 }
