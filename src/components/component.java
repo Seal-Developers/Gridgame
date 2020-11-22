fa008411b581189c92a507d02a69c55645aa8c23
Mexi (Meixuan Lu) <70993639+lumeixu1@users.noreply.github.com>
2020-11-22 15:18:55 -0500

    component updated

--- src/components/component.java
+++ src/components/component.java
@@ -1,9 +1,17 @@
 package components;
 
-
+/**
+ * Represents the abstract base of all Commands
+ *
+ */
 public abstract class component {
 	
 	protected int graphSize; //for one side
+	/**
+	 * update the current position field of the components
+	 * @param a an int represent the goal 1d position 
+	 * @param b an int represent another goal 1d position or just an unrelated default value
+	 */
 	public abstract void move(int a,int b);
 	
 }
